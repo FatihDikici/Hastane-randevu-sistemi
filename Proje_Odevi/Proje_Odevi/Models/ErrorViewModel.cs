d3c9b247983d1f6500e10285f6ef4f5bdee49a9a
Fatih Dikici <fatih.dikici5734@gmail.com>
2023-12-07 21:45:55 +0300

    1.Commit
    Proje dosyaları ve kullanıcı login sayfasının tasarım kısmı oluşturuldu.

---
+++ Proje_Odevi/Proje_Odevi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proje_Odevi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
