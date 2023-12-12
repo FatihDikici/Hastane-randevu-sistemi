adf35ccde70161dbd887d7f6c6ab3a99334a85ea
Fatih Dikici <fatih.dikici5734@gmail.com>
2023-12-12 14:47:16 +0300

    2.Commit
    Login sayfasının tasarımlarını ilerlettim ve bazı eklemeleri yaptım

---
+++ 2.Commit/Proje_Odevi/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Proje_Odevi.Models;
+
+namespace Proje_Odevi.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //login işlemleri
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
