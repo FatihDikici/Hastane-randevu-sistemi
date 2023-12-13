bc411dc147cb404d3940f333f455d9ec8c1f1bf5
Fatih Dikici <fatih.dikici5734@gmail.com>
2023-12-13 15:33:16 +0300

    3.Commit
    Register sayfa tasarımı eklendi,Veritabanı bağlantısı için Visual studio'dan gerekli paketler kuruldu.

---
+++ 3.Commit/Proje_Odevi/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Proje_Odevi.Models;
+using System.Reflection;
+
+namespace Proje_Odevi.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //login işlemleri
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Register()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //Register işlemleri
+            }
+
+            return View(model);
+
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
---
+++ 3.Commit/Proje_Odevi/Models/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proje_Odevi.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage ="Kullanıcı adınızı giriniz.")]
+        [StringLength(40,ErrorMessage ="Kullanıcı adı en fazla 40 karakter olabilir.")]
+        public string Username { get; set; }
+
+        //[DataType(DataType.Password)]
+        [Required(ErrorMessage ="Şifrenizi giriniz.")]
+        [MinLength(6,ErrorMessage ="En az 6 karakter girmek zorunludur.")]
+        public string Password { get; set; }
+
+
+    }
+}
