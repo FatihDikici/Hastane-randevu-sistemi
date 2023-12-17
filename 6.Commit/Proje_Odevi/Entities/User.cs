2578ed510cc706342965b59ceeb80226ef9dbaae
Fatih Dikici <fatih.dikici5734@gmail.com>
2023-12-17 13:57:44 +0300

    6.Commit
    Admin sayfası oluşturuldu ve kullanıcı ve admin sayfasına özellikler eklendi.

---
+++ 6.Commit/Proje_Odevi/Entities/User.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Proje_Odevi.Entities
+{
+    [Table("Kullanıcılar")]
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+
+        public string? NameSurname { get; set; } = null;
+        
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Password { get; set; }
+        public bool Locked { get; set; } = false;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        [Required]
+        public string Role { get; set; } = "User";
+    }
+}
