4663e1f3ce657f0921b47a329e299c45b404c269
Fatih Dikici <fatih.dikici5734@gmail.com>
2023-12-24 15:49:56 +0300

    10.Commit
    Yönetici yetkilerinde kullanıcıları düzenleme ve kaldırma işlemleri eklendi.

---
+++ 10.Commit/Proje_Odevi/Controllers/AccountController.cs
@@ -0,0 +1,201 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NETCore.Encrypt.Extensions;
+using Proje_Odevi.Entities;
+using Proje_Odevi.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Security.Claims;
+
+namespace Proje_Odevi.Controllers
+{
+
+    public class AccountController : Controller
+    {
+        private readonly DatabaseContext _databaseContext;
+        private readonly IConfiguration _configuration;
+
+
+        public AccountController(DatabaseContext databaseContext, IConfiguration configuration)
+        {
+            _databaseContext = databaseContext;
+            _configuration = configuration;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                string hashedPassword = DoMD5HashedString(model.Password);
+
+                User user = _databaseContext.Users.SingleOrDefault(x => x.Username.ToLower() == model.Username.ToLower()
+                && x.Password == hashedPassword);
+
+                if (user != null)
+                {
+                    if (user.Locked)
+                    {
+                        ModelState.AddModelError(nameof(model.Username), "user is locked ");
+                        return View(model);
+                    }
+
+                    List<Claim> claims = new List<Claim>();
+                    claims.Add(new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()));
+                    claims.Add(new Claim(ClaimTypes.Name, user.NameSurname ?? string.Empty));
+                    claims.Add(new Claim(ClaimTypes.Role, user.Role));
+                    claims.Add(new Claim("Username", user.Username));
+
+                    ClaimsIdentity identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                    ClaimsPrincipal principal = new ClaimsPrincipal(identity);
+                    HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+                    return RedirectToAction("Index", "home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Kullanıcı adı veya şifreniz hatalı.");
+                }
+
+            }
+
+            return View(model);
+        }
+
+        private string DoMD5HashedString(string a)
+        {
+            string md5salt = _configuration.GetValue<string>("AppSettings:MD5Salt");
+            string salted = a + md5salt;
+            string hashed = salted.MD5();
+            return hashed;
+        }
+
+        public IActionResult Register()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+                if (_databaseContext.Users.Any(x => x.Username.ToLower() == model.Username.ToLower()))
+
+                {
+                    ModelState.AddModelError(nameof(model.Username), "Kullanıcı adı mevcuttur");
+                    View(model);
+                }
+
+                string hashedPassword = DoMD5HashedString(model.Password);
+
+                User user = new()
+                {
+                    Username = model.Username,
+                    Password = hashedPassword
+                };
+
+                _databaseContext.Users.Add(user);
+                int affectedRowCount = _databaseContext.SaveChanges();
+                if (affectedRowCount == 0)
+                {
+                    ModelState.AddModelError("", "Hata var");
+                }
+                else
+                {
+                    return RedirectToAction(nameof(Login));
+                }
+            }
+            
+            return View(model);
+
+        }
+
+        public IActionResult Profile()
+        {
+            ProfileInfoLoader();
+            return View();
+        }
+
+        private void ProfileInfoLoader()
+        {
+            Guid userid = new Guid(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            User user = _databaseContext.Users.SingleOrDefault(x => x.Id == userid);
+            ViewData["FullName"] = user.NameSurname;
+            ViewData["ProfileImage"] = user.ProfileImageFileName;
+        }
+
+        [HttpPost]
+        public IActionResult ProfileChangeFullName([Required(ErrorMessage = "Ad soyad kısmı boş bırakılamaz!")] string fullname)
+        {
+
+            if (ModelState.IsValid)
+            {
+                Guid userid = new Guid(User.FindFirstValue(ClaimTypes.NameIdentifier));
+                User user = _databaseContext.Users.SingleOrDefault(x => x.Id == userid);
+                user.NameSurname = fullname;
+                _databaseContext.SaveChanges();
+                return RedirectToAction(nameof(Profile));
+            }
+            ProfileInfoLoader();
+            return View("Profile");
+        }
+        [HttpPost]
+        public IActionResult ProfileChangePassword([Required][MinLength(6, ErrorMessage = "Şifrenizi tekrar giriniz,en az 6 haneli olmalıdır!")] string password)
+        {
+
+            if (ModelState.IsValid)
+            {
+                Guid userid = new Guid(User.FindFirstValue(ClaimTypes.NameIdentifier));
+                User user = _databaseContext.Users.SingleOrDefault(x => x.Id == userid);
+
+                string hashedPassword = DoMD5HashedString(password);
+                user.Password = hashedPassword;
+                _databaseContext.SaveChanges();
+                ViewData["result"] = "Şifre Değiştirildi";
+
+            }
+            ProfileInfoLoader();
+            return View("Profile");
+
+        }
+        [HttpPost]
+        public IActionResult ProfileChangeImage([Required(ErrorMessage = "Lütfen dosya seçiniz!")] IFormFile file)
+        {
+
+            if (ModelState.IsValid)
+            {
+                Guid userid = new Guid(User.FindFirstValue(ClaimTypes.NameIdentifier));
+                User user = _databaseContext.Users.SingleOrDefault(x => x.Id == userid);
+
+                string filename = $"p_{userid}.jpg";
+                Stream stream = new FileStream($"wwwroot/uploads/{filename}", FileMode.OpenOrCreate);
+                file.CopyTo(stream);
+                stream.Close();
+                stream.Dispose();
+                user.ProfileImageFileName= filename;
+                _databaseContext.SaveChanges();
+                return RedirectToAction(nameof(Profile));
+
+
+            }
+            ProfileInfoLoader();
+            return View("Profile");
+        }
+            public IActionResult Logout()
+            {
+
+                HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+                return RedirectToAction(nameof(Login));
+            }
+
+        }
+    } 
+
---
+++ 10.Commit/Proje_Odevi/Models/UserviewModels.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proje_Odevi.Models
+{
+    public class UserModel
+    {
+       
+        public Guid Id { get; set; }
+
+        public string? NameSurname { get; set; } = null;
+       
+        public string Username { get; set; }
+        public bool Locked { get; set; } = false;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+     
+        public string ProfileImageFileName { get; set; } = "noimage.jpg";
+
+        public string Role { get; set; } = "User";
+    }
+    public class CreateUserModel
+    {
+        [Required(ErrorMessage = "Kullanıcı adınızı giriniz.")]
+        [StringLength(40, ErrorMessage = "Kullanıcı adı en fazla 40 karakter olabilir.")]
+        public string Username { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string NameSurname { get; set; }
+
+        public bool Locked { get; set; }
+
+        //[DataType(DataType.Password)]
+        [Required(ErrorMessage = "Şifrenizi giriniz.")]
+        [MinLength(3, ErrorMessage = "En az 3 karakter girmek zorunludur.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Şifrenizi tekrar giriniz.")]
+        [MinLength(3, ErrorMessage = "En az 3 karakter girmek zorunludur.")]
+        [Compare(nameof(Password))]
+        public string RePassword { get; set; }
+
+        public string Role { get; set; } = "User";
+    }
+    public class EditUserModel
+    {
+        [Required(ErrorMessage = "Kullanıcı adınızı giriniz.")]
+        [StringLength(40, ErrorMessage = "Kullanıcı adı en fazla 40 karakter olabilir.")]
+        public string Username { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string NameSurname { get; set; }
+
+        public bool Locked { get; set; }
+
+        public string Role { get; set; } = "User";
+    }
+
+}
